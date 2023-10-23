b30e7c57bf421fbac885c99792c118eec06577fa
Daniel Antônio <66747998+DANIELSOUZA-1@users.noreply.github.com>
2023-10-23 00:31:30 -0300

    profile page start

---
+++ src/pages/menu/MenuManager.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+
+
+function MenuManagerPage() {
+  let navigate = useNavigate()
+  return (
+    <div className='h-full justify-center pt-44 bg-white'>
+      
+    </div>
+  )
+}
+
+export default MenuManagerPage
