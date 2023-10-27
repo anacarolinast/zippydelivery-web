7825c236b0c6a60d07e12916b6cee5275974ad15
Daniel Antônio <66747998+DANIELSOUZA-1@users.noreply.github.com>
2023-10-27 09:57:31 -0300

    login e cadastro alteração

--- src/pages/auth/login/login.jsx
+++ src/pages/auth/login/login.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
 import LogoComponent from '../../components/logoComponent';
+import zippy from '../../../assets/img/zippy.png';
 
 
 function LoginPage() {
   let navigate = useNavigate()
   return (
-    <div className='flex flex-col gap-20 sm:flex-row items-center h-full'>
+    <div className='flex flex-col  sm:flex-row gap-14 items-center h-full'>
       {/* Lado esquerdo do login */}
-      <div className="flex h-[10%] gap-5 mt-8 items-center w-1/2 justify-center sm:-mt-28">
-        <LogoComponent></LogoComponent>
+      <div className="flex gap-5 items-center w-1/2  h-full bg-orange-100 justify-center ">
+        {/* Logo Branca */}
+        <div className='flex flex-col text-white items-center mb-14'>
+          <img className='scale-75 -mb-14 ' src={zippy} alt="" />
+          <div className='flex items-center gap-3 z-50'>
+            <div className="w-6 h-6 relative">
+              <div className="hexagon !bg-white"></div>
+            </div>
+            <div className="flex flex-col justify-center items-end mb-1">
+              <span className="text-3xl font-bold  lowercase">
+                Zippy Delivery
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
       {/* Lado direito do login */}
-      <div className='flex h-[90%] sm:h-full w-full sm:w-1/2 items-start sm:items-center justify-center'>
+      <div className='flex h-[90%] sm:h-screen  w-full sm:w-1/2 items-start sm:items-center justify-center'>
         <div className='flex w-full sm:w-[30rem] flex-col  gap-8 px-12 py-16 rounded-md shadow-xl bg-white'>
           <span className='text-3xl font-semibold mx-auto'>Faça login</span>
-          
+
           {/* Campos */}
           <div className='flex flex-col gap-6'>
             <div className='flex flex-col gap-1 text-gray-500' >
@@ -28,7 +42,7 @@ function LoginPage() {
               <input placeholder='No mínimo 6 caracteres' className='input' type="text" />
               <span className='text-secondary mt-1'>Esqueceu a senha ? <span onClick={() => { navigate('reset-password') }} className='text-gray-600 font-semibold cursor-pointer'>Recuperar Senha</span></span>
             </div>
-            
+
           </div>
 
           {/* Botões */}
