d6c8656a0d8886504664ed420a7cb333d0ec4665
Aluno <aluno@ifpe>
2023-11-07 10:47:52 -0300

    ajustes no front

--- src/pages/auth/login/login.jsx
+++ src/pages/auth/login/login.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
-import LogoComponent from '../../components/logoComponent';
+//import LogoComponent from '../../components/logoComponent';
 import zippy from '../../../assets/img/zippy.png';
 
 
 function LoginPage() {
   let navigate = useNavigate()
   return (
-    <div className='flex flex-col  sm:flex-row gap-14 items-center h-full'>
+    <div className='flex flex-col  sm:flex-row items-center h-full'>
       {/* Lado esquerdo do login */}
-      <div className="flex gap-5 items-center w-1/2  h-full bg-orange-100 justify-center ">
+      <div className="flex gap-5 items-center w-3/4  h-full bg-orange-100 justify-center ">
         {/* Logo Branca */}
         <div className='flex flex-col text-white items-center mb-14'>
           <img className='scale-75 -mb-14 ' src={zippy} alt="" />
@@ -26,28 +26,25 @@ function LoginPage() {
         </div>
       </div>
       {/* Lado direito do login */}
-      <div className='flex h-[90%] sm:h-screen  w-full sm:w-1/2 items-start sm:items-center justify-center'>
-        <div className='flex w-full sm:w-[30rem] flex-col  gap-8 px-12 py-16 rounded-md shadow-xl bg-white'>
-          <span className='text-3xl font-semibold mx-auto'>Faça login</span>
-
-          {/* Campos */}
-          <div className='flex flex-col gap-6'>
+      <div className='flex flex-col gap-20 px-12 py-16 h-[90%] sm:h-screen w-full sm:w-1/2 items-start sm:items-center justify-center bg-white shadow-xl'>
+        <span className='text-4xl font-semibold mx-auto text-gray-800'>Faça login</span>
+        <div className='flex flex-col gap-14 sm:w-1/2'>
+          <div className='flex flex-col gap-10'>
+            {/* Campos */}
             <div className='flex flex-col gap-1 text-gray-500' >
               <span className='input-label'>Email</span>
-              <input placeholder='Exemplo@exemplo.com.br' className='input ' type="text" />
+              <input placeholder='seuemail@exemplo.com.br' className='input ' type="text" />
             </div>
-
             <div className='flex flex-col gap-1 text-gray-500'>
               <span className='input-label'>Senha</span>
-              <input placeholder='No mínimo 6 caracteres' className='input' type="text" />
-              <span className='text-secondary mt-1'>Esqueceu a senha ? <span onClick={() => { navigate('reset-password') }} className='text-gray-600 font-semibold cursor-pointer'>Recuperar Senha</span></span>
+              <input placeholder='******' className='input' type="text" />
+              {/*  <span className='text-secondary mt-1'>Esqueceu a senha ? <span onClick={() => { navigate('reset-password') }} className='text-gray-600 font-semibold cursor-pointer'>Recuperar Senha</span></span> */}
             </div>
-
           </div>
 
           {/* Botões */}
-          <div className='flex flex-col gap-2'>
-            <button onClick={() => { navigate('init') }} className='primary-button'>Entrar</button>
+          <div className='flex flex-col gap-1'>
+            <button onClick={() => { navigate('init') }} className='primary-button font-semibold py-2'>Entrar</button>
             <button onClick={() => { navigate('sign-up') }} className='outline-button'>Criar uma conta</button>
           </div>
         </div>
